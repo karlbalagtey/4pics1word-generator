d20ddc3d9f8dc4d7e01b387b8e1d4488864fc65b
James Turner <jamesturnerpublic@gmail.com>
2013-02-14 19:31:11 +0000

    Second Commit

---
+++ generate.js
@@ -0,0 +1,69 @@
+self.addEventListener("message", function (e) {
+	// Get the data that was sent to the worker (dictionary, possible letters and number of spaces).
+	var dictionary = e.data.dict,
+		possibleLetters = e.data.letters,
+		spaces = e.data.spaces,
+		timeout = false,
+		lib = [];
+
+	// Keep generating more words until it times out (i.e. it runs out of combinations).
+	while ( timeout === false ) {
+		var word = generateWord();
+
+		// Check if the last word generated timed out.
+		if ( word.timeout == true ) timeout = true;
+
+		// Send the word and timeout status back to the main script.
+		self.postMessage({
+			"timeout" : timeout,
+			"word" : word.word
+		});
+	}
+
+	function generateWord () {
+		// By default it is assumed that the word generated is invalid (not in the dictionary).
+		var valid = false,
+			// Var to log the number of attempts at word generation which is used to timeout the script.
+			attempts = 0,
+			ret = {};
+
+		// Keep looping until we find a valid word and haven't timed out.
+		while ( valid === false && attempts <= 10000 ) {
+			// We need a temporary version of the 'letters' arrary, but arrays are passed by reference by default so we can slice the original as a work-around. 
+			var tempPossibleLetters = possibleLetters.slice(0),
+				word = "",
+				rand;
+
+			// Grab some letters from the temporary letters array to make a word of the correct length (defined by number of spaces).
+			for ( var i = 0; i < spaces; i++ ) {
+				rand = Math.floor(Math.random() * tempPossibleLetters.length);
+				word += tempPossibleLetters[rand];
+				// Now that we've used that letter we can't use it again and so remove it from the array.
+				tempPossibleLetters.splice(tempPossibleLetters.indexOf(tempPossibleLetters[rand]), 1);
+			}
+
+			// If the same word hasn't already been generated and is in the dictionary...
+			if ( lib.indexOf(word) == -1 && dictionary.indexOf(word) >= 0 ) {
+				// Put the newly generated word into our library of other generated words.
+				lib.push(word);
+
+				// Yay it's a valid word!
+				valid = true;
+
+				// Data to return to main script.
+				ret = {
+					"timeout" : false,
+					"word" : word
+				};
+				return ret;
+			}
+			attempts++;
+		}
+		// If we get to here, that means that either the word wasn't valid or we timed out.
+		ret = {
+			"timeout" : true,
+			"word" : null
+		}
+		return ret;
+	}
+});
---
+++ script.js
@@ -0,0 +1,113 @@
+(function() {
+	var inpLetters = document.getElementById("letters"),
+		inpSpaces = document.getElementById("spaces"),
+		btnExecute = document.getElementById("execute"),
+		ul = document.getElementById("possibleWordList"),
+		letters,
+		spaces,
+		possibleLetters = [];
+
+	// Loading (...) animation for the 'Generate' button.
+	function btnLoad (obj, action) {
+		if ( action == "start" ) {
+			// Remember what the old innerHTML was so we can restore it when the animation is complete.
+			var restoreHTML = obj.innerHTML;
+
+			// Set the 'data-status' attribute to 'inprogress' because we've just started the animation.
+			obj.setAttribute("data-status", "inprogress");
+
+			obj.setAttribute("disabled", true);
+
+			obj.innerHTML = "&middot;";
+
+			// Set up a 400ms timer that deals with the middot changes and checks the state of the animation.
+			var timer = setInterval(function () {
+				if ( obj.getAttribute("data-status") == "inprogress" ) {
+					// If the animation is still in progress add more middots.
+					if ( obj.innerHTML.length < 4 ) {
+						obj.innerHTML += "&middot;"
+					}
+					else {
+						obj.innerHTML = "&middot;";
+					}
+				}
+				// The animation has ended so stop the timer and restore the old innerHTML of the button.
+				else {
+					clearInterval(timer);
+					obj.innerHTML = restoreHTML;
+					obj.removeAttribute("disabled");
+					obj.removeAttribute("data-status");
+				}
+			}, 400);		
+		}
+		else if ( action == "complete" ) {
+			// Signify that the animation has completed so the timer will detect it on its next tick.
+			obj.setAttribute("data-status", "complete");
+		}
+	}
+
+
+	btnExecute.addEventListener("click", function () {
+		// Clear out any spaces on the 'letters' input.
+		letters = inpLetters.value.replace(/\s/g, "");
+		spaces = inpSpaces.value;
+
+		// Flush out the array that stores the possible letters so that old letters are removed if the user generates more words.
+		possibleLetters = [];
+
+		// Check that the user has filled out both text inputs and that the number of spaces isn't larger than the number of letters specified.
+		if ( letters.length < 1 ) {
+			inpLetters.focus();
+			return;
+		}
+		else if ( spaces.length < 1 || spaces > letters.length ) {
+			inpSpaces.focus();		
+			return;
+		}
+
+		var timeout = false;
+
+		// Disable the text inputs and start the loading animation.
+		inpLetters.setAttribute("disabled", true);
+		inpSpaces.setAttribute("disabled", true);
+		btnLoad(this, "start");
+
+		// Populate the 'letters' array with the letters from the text input.
+		for ( var i = 0; i < letters.length; i++ ) {
+			possibleLetters.push(letters.charAt(i));
+		}
+
+
+		// The web worker that will do the word generation.
+		var worker = new Worker("generate.js");
+
+		// Start the worker and hand it the dictionary, possible letters and the number of spaces.
+		worker.postMessage({
+			"dict" : dict,
+			"letters" : possibleLetters,
+			"spaces" : spaces
+		});
+
+		// Clear out the <ul> of any previously generated words
+		ul.innerHTML = "";
+		
+		// Listener for any messages sent back by the worker.
+		worker.addEventListener("message", function (e) {
+			// Get the data.
+			var response = e.data;
+			
+			// If the last word generation timed out, re-enable the text inputs and stop the loading animation.
+			if ( response.timeout == true ) {
+				inpLetters.removeAttribute("disabled");
+				inpSpaces.removeAttribute("disabled");
+				btnLoad(btnExecute, "complete");
+			}
+			// Or else, create a new <li> for the generated word and append it to the <ul>.
+			else {
+				var li = document.createElement("li");
+				li.innerHTML = response.word;
+				ul.appendChild(li);
+			}
+		}, false);			
+	});
+})();
